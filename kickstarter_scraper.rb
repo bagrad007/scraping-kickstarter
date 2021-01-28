653656f89bb5d133fadf9f2f0342a552e4801c94
Luke Gill <bagrad007@gmail.com>
2021-01-27 19:16:12 -0500

    commit tree de09d62f553f50333fca579e2c7858d73cc53610

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,12 +12,12 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      projects: kickstarter.css("li.project.grid_4").first 
-      title: project.css("h2.bbcard_name strong a").text 
-      image link: project.css("div.project-thumbnail a img").attribute("src").value 
-      description: project.css("p.bbcard_blurb").text
-      location: project.css("ul li a span.location-name").text
-      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      projects: kickstarter.css("li.project.grid_4").first,
+      title: project.css("h2.bbcard_name strong a").text,
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul li a span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
       
     }
   end
